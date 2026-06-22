3a1bd96128cd5ba6dfb47370e1ffb176c7fa7405
Intensed <intensed01@gmail.com>
2026-06-22 17:52:21 +0200

    Delete assets directory

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const script = document.createElement("script");
-  script.src = "https://unpkg.com/lucide@latest";
-  script.onload = () => {
-    lucide.createIcons();
-  };
-  document.body.appendChild(script);
-});
